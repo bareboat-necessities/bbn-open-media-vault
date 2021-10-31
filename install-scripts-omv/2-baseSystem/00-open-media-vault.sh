083b1bd2227c1a026a597af2e72a80f38382b1ea
mgrouch <mgrushinskiy@gmail.com>
2021-10-31 17:44:32 -0400

    initial

--- install-scripts-omv/2-baseSystem/00-open-media-vault.sh
+++ install-scripts-omv/2-baseSystem/00-open-media-vault.sh
@@ -9,7 +9,13 @@ export LC_CTYPE="C"
 export LC_MESSAGES="C"
 export LC_ALL="C"
 
+echo "samba-common samba-common/workgroup string WORKGROUP" | sudo debconf-set-selections
+echo "samba-common samba-common/dhcp boolean true" | sudo debconf-set-selections
+echo "samba-common samba-common/do_debconf boolean true" | sudo debconf-set-selections
+
 apt-get install -y -q chrony samba nginx-full php-common python3 collectd nfs-kernel-server beep
+
+install -m 755 -d -o root -g adm "/var/log/samba"
 
 chmod +x $FILE_FOLDER/install-omv.sh
 
